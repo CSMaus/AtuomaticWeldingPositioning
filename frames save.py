2b533067b020e2e900c8bd75144ab7c300180655
Kseniia Barashok <cs-maus-1@mail.ru>
2025-07-23 16:58:14 +0900

    save selected

--- frames save.py
+++ frames save.py
@@ -2,24 +2,33 @@
 import cv2
 import os
 
-videos_path = "D:/work_doks/projects/Doosan. Welding/2025/data/"
-video_name = os.listdir(videos_path)[11]
+# videos_path = "D:/work_doks/projects/Doosan. Welding/2025/data/"
+videos_path = "D:/ML_DL_AI_stuff/!!DoosanWelding2025/data/basler_recordings/"
+save_path = "D:/ML_DL_AI_stuff/!!DoosanWelding2025/data/basler_recordings_frames/"
+video_name = os.listdir(videos_path)[5]
 this_video_path = os.path.join(videos_path, video_name)
-output_path = f"dataset/frames/{video_name[:-4]}"
+print("Output path: ", save_path)
+# f"dataset/frames/{video_name[:-4]}"
 
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
 
 cap = cv2.VideoCapture(this_video_path)
 
-frame_id = 0
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    if frame_id % 5 == 0:
-        cv2.imwrite(f"{output_path}/frame_{frame_id:04d}.jpg", frame)
-    frame_id += 1
+count = 0
+while cap.isOpened():
+    ok, frame = cap.read()
+    if not ok: break
 
-print(f"Saved {frame_id // 5 + 1} frames.")
+    cv2.imshow("video", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('s'):
+        cv2.imwrite(f"{save_path}/{video_name[:-4]}-frame_{count:04d}.png", frame)
+        print("Saved to: ", f"{save_path}/{video_name[:-4]}-frame_{count:04d}.png")
+        count += 1
+    elif key == ord('q'):
+        break
+
+print(f"saved {count} frames")
 cap.release()
+cv2.destroyAllWindows()
