f1878a6fa273bf635ebd78c5e42c5962f9ed2a45
Kseniia Barashok <cs-maus-1@mail.ru>
2025-09-09 15:12:18 +0900

    correct frames name

--- frames save.py
+++ frames save.py
@@ -4,7 +4,7 @@ import os
 
 # videos_path = "D:/work_doks/projects/Doosan. Welding/2025/data/"
 videos_path = "D:/ML_DL_AI_stuff/!!DoosanWelding2025/data/basler_recordings/"
-save_path = "D:/ML_DL_AI_stuff/!!DoosanWelding2025/data/basler_recordings_frames/"
+save_path = "D:/ML_DL_AI_stuff/!!DoosanWelding2025/data/br_frames-0909/"
 video_name = os.listdir(videos_path)[5]
 this_video_path = os.path.join(videos_path, video_name)
 print("Output path: ", save_path)
@@ -16,6 +16,7 @@ if not os.path.exists(save_path):
 cap = cv2.VideoCapture(this_video_path)
 
 count = 0
+saved_num = 0
 while cap.isOpened():
     ok, frame = cap.read()
     if not ok: break
@@ -25,10 +26,12 @@ while cap.isOpened():
     if key == ord('s'):
         cv2.imwrite(f"{save_path}/{video_name[:-4]}-frame_{count:04d}.png", frame)
         print("Saved to: ", f"{save_path}/{video_name[:-4]}-frame_{count:04d}.png")
-        count += 1
+        saved_num += 1
+
     elif key == ord('q'):
         break
+    count += 1
 
-print(f"saved {count} frames")
+print(f"saved {saved_num} frames")
 cap.release()
 cv2.destroyAllWindows()
